acadf310e4995a29289117cefe39e54b69994816
owaizmajoriya01 <117156815+owaizmajoriya01@users.noreply.github.com>
2025-03-15 14:13:21 -0500

    Add files via upload

---
+++ to-do_list_app.py
@@ -0,0 +1,40 @@
+tasks=[]
+
+if __name__=="__main__":
+    print("Welcome to the to-do list App!")
+    
+    while True:
+        print("\n\n")
+        print("please select one of the following options")
+        print("------------------------------------------- ")
+        print("1. Add a new task")
+        print("2. delete a task")
+        print("3. List Tasks")
+        print("4. Quit")
+
+        choice = input("\nEnter your choice:  ")
+
+        if (choice =="1"):
+            print("\nyou selected to add a task!")
+            task = input("write a task: ")
+            tasks.append(task)
+            print("\nTask added successfully")
+
+        elif (choice == "2"):
+            count=int(input("enter the # of the task to delete: "))
+            if count > len(tasks) or count < 0 :
+                print("invalid # number")
+            else:
+                print("your want to remove the task "+tasks[count-1])
+                tasks.pop(count-1)
+                print("tasks removed successfully!")
+
+
+        elif (choice == "3"):
+            print("you selected to lists all the task: ")
+            for task in tasks:
+                print(task)
+        elif (choice == "4"):
+            break
+        else:
+            print("invalid input, please try again!")
